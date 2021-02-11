710971eec56a413d586756377eae31dd56d02aab
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-02-10 22:12:58 -0800

    Added card design file

--- src/components/SubscriptionCard.js
+++ src/components/SubscriptionCard.js
@@ -7,10 +7,13 @@ const SubscriptionCard = () => {
         <div className="title-container">
           <img src="https://via.placeholder.com/150" id="card-img" />{" "}
           <h2 className="title">Netflix</h2>
-          <h4 className="due-date">DUE IN 3 DAYS</h4>
+          <div className="due-date">
+            <h4>6</h4>
+            <h4>Days</h4>
+          </div>
         </div>
         <div className="cost-container">
-          <h4 className="due-date">Due Date: 11/18/2020</h4>
+          <h4 className="due-date">Due Date: 02/18/2021</h4>
           <h4 className="cost">Cost: $14.99</h4>
           <p>Netflix Standard plan started 09/18/2020</p>
         </div>
