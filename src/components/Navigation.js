89b6737684351feed8931b371059b67697ad13ec
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-03-02 21:02:03 -0800

    Updated Nav and Logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import Logo from "../assets/img/logo.jpg";
+import Logo from "../assets/img/logo2.jpg";
 
 const Navigation = (props) => {
   const { user } = props;
